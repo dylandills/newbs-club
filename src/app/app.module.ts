79dc284524ad6b3c7a342a11922afcb95836e0ec
Dylan Dills <dylan.dills@gmail.com>
2017-07-14 18:25:16 -0700

    database configured, initalized, and connected to application via Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,16 @@ import { LastNamePipe } from './last-name.pipe';
 import { AboutComponent } from './about/about.component';
 import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
